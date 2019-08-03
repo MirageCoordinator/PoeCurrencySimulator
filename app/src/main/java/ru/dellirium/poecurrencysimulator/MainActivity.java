bd92a0db3c50b91073bdc905e03cc957346b6b18
Dellirium <dellirium@dellirium.ru>
2019-08-04 01:37:54 +0300

    deleted unnecessary imports

--- app/src/main/java/ru/dellirium/poecurrencysimulator/MainActivity.java
+++ app/src/main/java/ru/dellirium/poecurrencysimulator/MainActivity.java
@@ -1,15 +1,9 @@
 package ru.dellirium.poecurrencysimulator;
 
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.GridLayout;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
